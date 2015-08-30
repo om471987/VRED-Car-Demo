98b8f80817e0132ab38d285ee1836be3a640f429
Omkar Panhalkar <omkar.panhalkar@sonydadc.com>
2015-08-29 19:13:43 -0700

    first commit

---
+++ VREDInterface/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Windows;
+
+namespace VREDInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _vredServerUrl = ConfigurationManager.AppSettings["VREDServerUrl"];
+        private string _pythonScript = ConfigurationManager.AppSettings["PythonScript"];
+        private string _baseLoad = "{0}/python?value=load(\"{1}\")";
+        private string _baseEval = "{0}/pythoneval?value={1}";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            UrlTextBox.Text = _vredServerUrl;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            var url = string.Format(_baseLoad, UrlTextBox.Text, _pythonScript);
+            if (string.IsNullOrWhiteSpace(url) &&  !IsValidUrl(url))
+            {
+                MessageBox.Show("Please enter valid url");
+            }
+            else
+            {
+                WheelbaseSlider.Value = 0;
+                SendMessageToVRED(url);
+            }
+        }
+
+        private void WheelbaseSlider_Changed(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var command = @"updateWheelBase(" + WheelbaseSlider.Value + ")";
+            var url = string.Format(_baseEval, UrlTextBox.Text, command);
+            WheelbaseValue.Content = WheelbaseSlider.Value;
+            SendMessageToVRED(url);
+        }
+        private void SendMessageToVRED(string url)
+        {
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                httpWebRequest.KeepAlive = false;
+                var httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                httpWebResponse.Close();
+            }
+            catch (WebException exception)
+            {
+                var responseMessage = "";
+                if (exception != null && exception.Response != null)
+                {
+                    responseMessage = new StreamReader(exception.Response.GetResponseStream()).ReadToEnd();
+                }
+                else
+                {
+                    responseMessage = exception.ToString();
+                }
+                //MessageBox.Show("Error while messaging VRED. Please check the settings. Details - " + responseMessage);
+            }
+        }
+
+        private bool IsValidUrl(string url)
+        {
+            Uri uriResult;
+            bool result = Uri.TryCreate(url, UriKind.Absolute, out uriResult) && uriResult.Scheme == Uri.UriSchemeHttp;
+            return result;
+        }
+    }
+}
---
+++ VREDInterface/Scripts/carMovement.py
@@ -0,0 +1,215 @@
+print "Executing find script!"
+
+newScene()
+loadGeometry("car.osb")
+#nodes
+car = findNode("car")
+wheelBase_Front = findNode("WheelBase_Front")
+wheelBase_Back = findNode("WheelBase_Back")
+wheelBase = findNode('Skirt_MultiMaterial')
+tireFL = findNode("TireFL_MultiMaterial")
+tireRimFL = findNode("TireFL_mat_0")
+tireRL = findNode("TireRL_MultiMaterial")
+tireRimRL = findNode("TireRL_mat_0")
+tireFR = findNode("TireFR_MultiMaterial")
+tireRimFR = findNode("TireFR_mat_0")
+tireRR = findNode("TireRR_MultiMaterial")
+tireRimRR = findNode("TireRR_mat_0")
+steeringWheel = findNode("Steering_Transform")
+puddles = findNodes('puddleCone',True)
+
+#materials
+waterColorMaterial = findMaterial("WaterColor")
+tireMaterial = findMaterial("Tire")
+
+updateScene()
+
+from time import sleep
+from math import sin, cos, pow
+
+#camera
+firstPersonCamera = [10, 30, 30]
+thirdPersonCamera = [0,100,50]
+camera = [[], []]
+upVector = [-0.346251, 0.346252, 0.871906]
+
+#car
+carPosition = [0, 0]
+carAngle = [0]
+
+#tire
+tireAxis = [2.00, 2.00]
+tireSpeed = [15.0]
+tireAngleLeft = [-90]
+tireAngleRight = [90]
+
+#Steering Wheel
+steetingAngle = [4.16338e-006, 0.347707, 2.95671]
+
+#Wheelbase
+wheelBasePosition = [25.40, 25.40, 25.40]
+wheelBaseDelta = [0.0]
+
+#puddle
+puddleRadius = [80.0]
+
+def updateCameraPosition():
+	setFromAtUp(-1, carPosition[0] + camera[0][0], carPosition[1] + camera[0][1], camera[0][2], camera[1][0], camera[1][1], camera[1][2], upVector[0], upVector[1], upVector[2])
+	
+
+def updateCarPosition():
+	setTransformNodeTranslation(car, carPosition[0], carPosition[1], 0, True)
+	updateCameraPosition()
+	isTireOnPuddle()
+	print "getPositions", getTransformNodeTranslation(car, true)
+
+
+def updateCarAngle():
+	car.setRotation(0, 0, carAngle[0])
+	updateCameraPosition()
+	print "getRotation", car.getRotation()
+
+
+def updateTireRotation():
+	setTransformNodeRotation(tireFL, 1.0, tireAxis[0], -90.0)
+	setTransformNodeRotation(tireRL, 1.0, tireAxis[0], -90.0)
+	setTransformNodeRotation(tireFR, 1.0, tireAxis[1], 90.0)
+	setTransformNodeRotation(tireRR, 1.0, tireAxis[1], 90.0)
+
+def updateTireAngle():
+	tireFL.setRotation(1, 1, tireAngleLeft[0])
+	tireRL.setRotation(1, 1, tireAngleLeft[0])
+	tireFR.setRotation(1, 1, tireAngleRight[0])
+	tireRR.setRotation(1, 1, tireAngleRight[0])	
+
+
+def updateSteeringAngle():
+	setTransformNodeRotation(steeringWheel, steetingAngle[0], 0, 90)
+	
+def translateFront():
+	carPosition[0] -= 1 * sin(carAngle[0])
+	carPosition[1] -= 1 * cos(carAngle[0])
+	tireAxis[0] += tireSpeed[0]
+	tireAxis[1] -= tireSpeed[0]	
+	updateCarPosition()
+	updateTireRotation()
+
+
+def translateBack():
+	carPosition[0] += 1 * sin(carAngle[0])
+	carPosition[1] += 1 * cos(carAngle[0])
+	tireAxis[0] -= tireSpeed[0]
+	tireAxis[1] += tireSpeed[0]	
+	updateCarPosition()
+	updateTireRotation()
+
+
+def rotateLeft():
+	carAngle[0] += 1
+	steetingAngle[0] += 1
+	tireAngleLeft[0] += 1
+	tireAngleRight[0] += 1
+	updateCarAngle()
+	updateTireAngle()
+	updateSteeringAngle()
+
+
+def rotateRight():
+	carAngle[0] -= 1
+	steetingAngle[0] -= 1
+	tireAngleLeft[0] -= 1
+	tireAngleRight[0] -= 1	
+	updateCarAngle()
+	updateTireAngle()
+	updateSteeringAngle()
+
+def isTireOnPuddle():
+	locationFL = getTransformNodeTranslation(tireFL, true)
+	locationRL = getTransformNodeTranslation(tireRL, true)
+	locationFR = getTransformNodeTranslation(tireFR, true)
+	locationRR = getTransformNodeTranslation(tireRR, true)
+	
+	for puddle in puddles:
+		puddleLocation = getTransformNodeTranslation(puddle, true)
+		if (pow((locationFL.x() - puddleLocation.x()), 2) + pow((locationFL.y() - puddleLocation.y()), 2)) <= pow(puddleRadius[0], 2):
+			soakTireFL()
+		else:
+			dryUpTireFL()
+		if (pow((locationRL.x() - puddleLocation.x()), 2) + pow((locationRL.y() - puddleLocation.y()), 2)) <= pow(puddleRadius[0], 2):
+			soakTireRL()
+		else:
+			dryUpTireRL()
+		if (pow((locationFR.x() - puddleLocation.x()), 2) + pow((locationFR.y() - puddleLocation.y()), 2)) <= pow(puddleRadius[0], 2):
+			soakTireFR()
+		else:
+			dryUpTireFR()
+		if (pow((locationRR.x() - puddleLocation.x()), 2) + pow((locationRR.y() - puddleLocation.y()), 2)) <= pow(puddleRadius[0], 2):
+			soakTireRR()
+		else:
+			dryUpTireRR()
+			
+	
+def soakTireFL():
+	tireRimFL.setMaterial(waterColorMaterial)
+def soakTireFR():
+	tireRimFR.setMaterial(waterColorMaterial)
+def soakTireRL():	
+	tireRimRL.setMaterial(waterColorMaterial)
+def soakTireRR():	
+	tireRimRR.setMaterial(waterColorMaterial)
+	
+def dryUpTireFL():
+	tireRimFL.setMaterial(tireMaterial)
+def dryUpTireFR():	
+	tireRimFR.setMaterial(tireMaterial)
+def dryUpTireRL():
+	tireRimRL.setMaterial(tireMaterial)
+def dryUpTireRR():
+	tireRimRR.setMaterial(tireMaterial)
+	
+def changeView():
+	if camera[0] is thirdPersonCamera:
+		camera[0] = firstPersonCamera
+		camera[1] = [carPosition[0], carPosition[1] - 20, 20]
+		print "First Camera View"
+	else:
+		camera[0] = thirdPersonCamera
+		camera[1] = [carPosition[0], carPosition[1], 0]
+		print "Third Camera View"
+	updateCameraPosition()
+
+def updateWheelBase(sliderValue):
+	carPosition = [0, 0]
+	carAngle = [0]
+	
+	delta = sliderValue - wheelBaseDelta[0]
+	wheelBaseDelta[0] = sliderValue
+	
+	a = getTransformNodeTranslation(wheelBase_Front, true)
+	setTransformNodeTranslation(wheelBase_Front, a.x(), a.y() - (delta / 2), a.z(), True)
+
+	a = getTransformNodeTranslation(wheelBase_Back, true)
+	setTransformNodeTranslation(wheelBase_Back, a.x(), a.y() + (delta / 2), a.z(), True)
+	
+	wheelBase.setScale(wheelBasePosition[0] + wheelBaseDelta[0], wheelBasePosition[1], wheelBasePosition[2])
+	print wheelBase.getScale()
+	
+keyW = vrKey(Key_W)
+keyW.connect(translateFront)
+
+keyS = vrKey(Key_S)
+keyS.connect(translateBack)
+
+keyA = vrKey(Key_A)
+keyA.connect(rotateLeft)
+
+keyD = vrKey(Key_D)
+keyD.connect(rotateRight)
+
+keyC = vrKey(Key_C)
+keyC.connect(changeView)
+
+changeView()
+updateCarPosition()
+
+print("this should come on log")
