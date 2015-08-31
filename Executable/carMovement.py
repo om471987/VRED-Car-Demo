fa34535e5f58f42622bf8dad53a97b15a3f3b8ae
Omkar Panhalkar <omkar.panhalkar@sonydadc.com>
2015-08-31 05:45:10 -0700

    FPS works like a charm

--- Executable/carMovement.py
+++ Executable/carMovement.py
@@ -23,8 +23,8 @@ waterColorMaterial = findMaterial("WaterColor")
 tireMaterial = findMaterial("Tire")
 
 #camera
-firstPersonCamera = [10.0, 40.0, 30.0]
-thirdPersonCamera = [150.0, 30.0, 50.0]#angle constant and z axis
+firstPersonCamera = [180.0, 5.0, 35.0]
+thirdPersonCamera = [150.0, 22.0, 50.0]#angle, constant and z axis
 camera = [[]]
 
 #car
@@ -36,7 +36,7 @@ tireAxis = [2.00, 2.00]
 tireSpeed = [15.0]
 tireAngleLeft = [-90]
 tireAngleRight = [90]
-tireTurnSpeed = [.6]
+tireTurnSpeed = [.4]
 
 #Steering Wheel
 steeringAngle = list(steeringWheel.getRotation())
@@ -47,10 +47,12 @@ wheelBasePosition = [25.40, 25.40, 25.40]
 wheelBaseDelta = [0.0]
 
 #puddle
-newPuddle = puddles[0].copy()
-newPuddle.setName("puddleCone" + str(len(puddles) + 1))
-puddles.append(newPuddle)
-newPuddle.setTranslation(200, 200, 0)
+puddleAxis = [[0, 400], [400,0], [-400,0], [200,200], [-200,200],[200,-200], [-200,-200]]
+for i, t in enumerate(puddleAxis):
+	newPuddle = puddles[0].copy()
+	newPuddle.setName("puddleCone" + str(i + 2))
+	newPuddle.setTranslation(t[0], t[1], 0)
+	puddles.append(newPuddle)
 
 collisionTireFL = vrCollision([tireFL], puddles)
 collisionTireFR = vrCollision([tireFR], puddles)
@@ -60,15 +62,14 @@ collisionTireRR = vrCollision([tireRR], puddles)
 from time import sleep
 from math import sin, cos, radians
 
-'''def updateCamera():
-	carTra = car.getTranslation()
-	carRot = car.getRotation()
-	x = camera[0][0] + carSpeed[0] *  sin(radians(rotation))
-	y = camera[0][1] - carSpeed[0] *  cos(radians(rotation))	
-	print x, ",", y
-	fromPtr = Pnt3f(carTra[0] + x, carTra[1] + y, camera[0][2])
-	toPtr = Pnt3f(carTra[0], carTra[1], camera[0][2])
-	setFromAtUp(-1, fromPtr, toPtr, getUp(-1))'''
+def updateCamera(x, y, rotation):
+	aa = x + carSpeed[0] * camera[0][1] * sin(radians(rotation + (camera[0][0])))
+	bb = y - carSpeed[0] * camera[0][1] * cos(radians(rotation + (camera[0][0])))
+	fromPtr = Pnt3f(aa, bb, camera[0][2])
+	firstX = x + camera[0][1] * sin(radians(rotation + (camera[0][0])))
+	firstY = y - camera[0][1] * cos(radians(rotation + (camera[0][0])))
+	toPtr = Pnt3f(firstX, firstY, camera[0][2])
+	setFromAtUp(-1, fromPtr, toPtr, getUp(-1))	
 
 
 def aswd(anglularForce, translateForce):
@@ -92,12 +93,7 @@ def aswd(anglularForce, translateForce):
 		tireAxis[1] += -1 * translateForce * tireSpeed[0]		
 		car.setTranslation(x, y, 0)
 		updateTireRotation()
-		
-	aa = x + carSpeed[0] * 1 * camera[0][1] * sin(radians(rotation + (camera[0][0])))
-	bb = y - carSpeed[0] * 1 * camera[0][1] * cos(radians(rotation + (camera[0][0])))
-	fromPtr = Pnt3f(camera[0][0] + aa, camera[0][1] + bb, camera[0][2])
-	toPtr = Pnt3f(x, y, camera[0][2])
-	setFromAtUp(-1, fromPtr, toPtr, getUp(-1))	
+	updateCamera(x, y, rotation)
 	isTireOnPuddle()
 
 def updateTireRotation():
@@ -145,12 +141,8 @@ def changeView():
 	else:
 		camera[0] = thirdPersonCamera
 		print "Third Camera View"
-	aa = camera[0][1] * sin(radians(camera[0][0]))
-	bb = camera[0][1] * cos(radians(camera[0][0]))
-	fromPtr = Pnt3f(camera[0][0] + aa, camera[0][1] + bb, camera[0][2])
-	cc = car.getTranslation()
-	toPtr = Pnt3f(cc[0], cc[1], camera[0][2])
-	setFromAtUp(-1, fromPtr, toPtr, getUp(-1))
+	rotation = car.getRotation()[2]
+	aswd(0.0, 1.0)
 
 def updateWheelBase(sliderValue):
 	delta = sliderValue - wheelBaseDelta[0]
@@ -159,48 +151,29 @@ def updateWheelBase(sliderValue):
 	#x = a[0] + sin(radians(carRot[2]))
 	#y = a[1] + cos(radians(carRot[2]))
 	
-	a = getTransformNodeTranslation(wheelBase_Front, true)
-	setTransformNodeTranslation(wheelBase_Front, a.x(), a.y() - (delta / 2), a.z(), True)
+	aa = getTransformNodeTranslation(wheelBase_Front, true)
+	setTransformNodeTranslation(wheelBase_Front, aa.x(), aa.y() - (delta / 2), aa.z(), True)
 
-	a = getTransformNodeTranslation(wheelBase_Back, true)
-	setTransformNodeTranslation(wheelBase_Back, a.x(), a.y() + (delta / 2), a.z(), True)
+	aa = getTransformNodeTranslation(wheelBase_Back, true)
+	setTransformNodeTranslation(wheelBase_Back, aa.x(), aa.y() + (delta / 2), aa.z(), True)
 
 	wheelBase.setScale(wheelBasePosition[0] + wheelBaseDelta[0], wheelBasePosition[1], wheelBasePosition[2])
 	
 w = vrKey(Key_W)
 w.connect(aswd, 0.0, 1.0)
-up = vrKey(Key_Up)
-up.connect(aswd, 0.0, 1.0)
 
 s = vrKey(Key_S)
 s.connect(aswd, 0.0, -1.0)
-down = vrKey(Key_Down)
-down.connect(aswd, 0.0, -1.0)
 
 d = vrKey(Key_D)
 d.connect(aswd, -1.0, 0.0)
-left = vrKey(Key_Left)
-left.connect(aswd, -1.0, 0.0)
 
 a = vrKey(Key_A)
 a.connect(aswd, 1.0, 0.0)
-right = vrKey(Key_Right)
-right.connect(aswd, 1.0, 0.0)
-
 
 keyC = vrKey(Key_C)
 keyC.connect(changeView)
 
 changeView()
-
-'''def cameraAngle(x):
-	camera[0][0] +=  x
-	updateCamera()
-
-key1 = vrKey(Key_1)
-key1.connect(cameraAngle, 1)
-
-key2 = vrKey(Key_2)
-key2.connect(cameraAngle, -1)'''
-
+	
 print("this should come on log")
